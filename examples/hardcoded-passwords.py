db1a78d4de60e365b783386ead3612e5f8a50c7a
Travis McPeak <travis.mcpeak@hp.com>
2014-09-16 16:02:20 -0700

    Bug fix for hardcoded passwords test

--- examples/hardcoded-passwords.py
+++ examples/hardcoded-passwords.py
@@ -5,3 +5,11 @@ def someFunction2(password):
     if password == "root":
         print "OK, logged in"
 
+def noMatch(password):
+    if password == '':
+        print "No password!"
+
+def NoMatch2(password):
+    if password == "ajklawejrkl42348swfgkg":
+        print "Nice password!"
+
