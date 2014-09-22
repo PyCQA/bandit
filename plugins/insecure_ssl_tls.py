c819a3f32d99390eadc167c86cc756b12e1060ce
Tim Kelsey <tim.kelsey@hp.com>
2014-09-22 14:29:31 +0100

    Adding SSL/TLS protocol version checks
    Bandit can now check for insecure SSL/TLS versions, bad versions
    are defined in the config file. By default:
     - SSLv2 is bad
     - SSLv23 is bad
     - SSLv3 is bad
     - TLSv1 is bad
    
    This check works with ssl and pyOpenSSL modules.
    A second check will also catch default use of ssl.wrap_socket as
    this will use SSLv23 if no option is given. This is an INFO level
    check.
    
    Change-Id: I5ff8ea7038eed41b09e4d0ebfcb59bfd7fd4c894

---
+++ plugins/insecure_ssl_tls.py
@@ -0,0 +1,58 @@
+# -*- coding:utf-8 -*-
+#
+# Copyright 2014 Hewlett-Packard Development Company, L.P.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import bandit
+from bandit.test_selector import *
+
+
+def get_bad_proto_versions(config):
+    return config['bad_protocol_versions']
+
+@takes_config
+@checks_functions
+def ssl_with_bad_version(context, config):
+    bad_ssl_versions = get_bad_proto_versions(config)
+    if (context.call_function_name_qual == 'ssl.wrap_socket'):
+        if context.check_call_arg_value('ssl_version') in bad_ssl_versions:
+
+            return(bandit.ERROR, 'ssl.wrap_socket call with insecure SSL/TLS'
+                   ' protocol version identified, security issue.  %s' %
+                   context.call_args_string)
+    elif (context.call_function_name_qual == 'SSL.Context'):
+        if context.check_call_arg_value('method') in bad_ssl_versions:
+
+            return(bandit.ERROR, 'SSL.Context call with insecure SSL/TLS'
+                   ' protocol version identified, security issue.  %s' %
+                   context.call_args_string)
+
+    elif (context.call_function_name_qual != 'ssl.wrap_socket' and
+       context.call_function_name_qual != 'SSL.Context'):
+        if (context.check_call_arg_value('method') in bad_ssl_versions or
+           context.check_call_arg_value('ssl_version') in bad_ssl_versions):
+
+            return(bandit.WARN, 'Function call with insecure SSL/TLS '
+                   'protocol identified, possible security issue.  %s' %
+                   context.call_args_string)
+
+@checks_functions
+def ssl_with_no_version(context):
+    if (context.call_function_name_qual == 'ssl.wrap_socket'):
+        if context.check_call_arg_value('ssl_version') is None:
+
+            return(bandit.INFO, 'ssl.wrap_socket call with no SSL/TLS'
+                   ' protocol version specified, the default SSLv23 could be'
+                   ' insecure, possible security issue.  %s' %
+                   context.call_args_string)
