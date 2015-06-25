059bd48cfb4c707acff2ebc0fef75caef82718de
Ian Cordasco <ian.cordasco@rackspace.com>
2015-06-24 19:48:56 -0500

    Actually rely on entry-points for formatters
    This should have been included in
    I102277dcd9481f2573028a436e910eda10011d91 but somehow the code was
    re-added in a later patchset. The way we picked our formatter function
    prior to this was a bit too clever. Checking if the report_func is
    self._report_text would not work and cause the format to always be 'txt'
    even when an output file was supplied. To reproduce this try the
    following:
    
        >>> class A(object):
        ...     def foo(self, bar):
        ...         pass
        ...
        >>> a = A()
        >>> b = a.foo
        >>> b is a.foo
        False
    
    By properly using our extension manager, we can more simply check the
    name of the formatter used and thus side-step this mess more easily.
    
    Closes-bug: 1468075
    Change-Id: I8e48cf40b2ef200b53e259ecf9ae54973b1aa0fb

--- bandit/core/result_store.py
+++ bandit/core/result_store.py
@@ -17,17 +17,11 @@
 
 """An object to store/access results associated with Bandit tests."""
 
-from collections import defaultdict
 from collections import OrderedDict
-import csv
-from datetime import datetime
-import json
 import linecache
-from operator import itemgetter
-
-import six
 
 from bandit.core import constants
+from bandit.core import extension_loader
 from bandit.core import utils
 
 
@@ -86,236 +80,20 @@ class BanditResultStore():
 
         self.count += 1
 
-    def _report_xml(self, file_list, scores, excluded_files):
-        '''Prints/returns warnings in XML format (Xunit compatible)
-
-        :param files_list: Which files were inspected
-        :param scores: The scores awarded to each file in the scope
-        :param excluded_files: Which files were excluded from the scope
-        :return: A collection containing the XML data
-        '''
-
-        import xml.etree.cElementTree as ET
-
-        if self.out_file is None:
-            self.out_file = 'bandit_results.xml'
-
-        items = self.resstore.items()
-        root = ET.Element('testsuite', name='bandit', tests=str(len(items)))
-        for filename, issues in items:
-            for issue in issues:
-                test = issue['test']
-                testcase = ET.SubElement(root, 'testcase',
-                                         classname=filename, name=test)
-                if self._check_severity(issue['issue_severity']):
-                    text = 'Severity: %s Confidence: %s\n%s\nLocation %s:%s'
-                    text = text % (
-                        issue['issue_severity'], issue['issue_confidence'],
-                        issue['issue_text'], issue['fname'], issue['lineno'])
-                    ET.SubElement(testcase, 'error',
-                                  type=issue['issue_severity'],
-                                  message=issue['issue_text']).text = text
-
-        tree = ET.ElementTree(root)
-        tree.write(self.out_file, encoding='utf-8', xml_declaration=True)
-
-        print("XML output written to file: %s" % self.out_file)
-
-    def _report_csv(self, file_list, scores, excluded_files):
-        '''Prints/returns warnings in JSON format
-
-        :param files_list: Which files were inspected
-        :param scores: The scores awarded to each file in the scope
-        :param excluded_files: Which files were excluded from the scope
-        :return: A collection containing the CSV data
-        '''
-
-        results = self._get_issue_list()
-
-        # Remove the code from all the issues in the list, as we will not
-        # be including it in the CSV data.
-        def del_code(issue):
-            del issue['code']
-        map(del_code, results)
-
-        if self.out_file is None:
-            self.out_file = 'bandit_results.csv'
-
-        with open(self.out_file, 'w') as fout:
-            fieldnames = ['filename',
-                          'test_name',
-                          'issue_severity',
-                          'issue_confidence',
-                          'issue_text',
-                          'line_number',
-                          'line_range']
-
-            writer = csv.DictWriter(fout, fieldnames=fieldnames,
-                                    extrasaction='ignore')
-            writer.writeheader()
-            for result in results:
-                writer.writerow(result)
-
-        print("CSV output written to file: %s" % self.out_file)
-
-    def _report_json(self, file_list, scores, excluded_files):
-        '''Prints/returns warnings in JSON format
-
-        :param files_list: Which files were inspected
-        :param scores: The scores awarded to each file in the scope
-        :param excluded_files: Which files were excluded from the scope
-        :return: JSON string
-        '''
-
-        stats = dict(zip(file_list, scores))
-
-        machine_output = dict({'results': [], 'errors': [], 'stats': []})
-        collector = list()
-        for (fname, reason) in self.skipped:
-            machine_output['errors'].append({'filename': fname,
-                                            'reason': reason})
-
-        for filer, score in six.iteritems(stats):
-            totals = {}
-            for i in range(self.level, len(constants.RANKING)):
-                severity = constants.RANKING[i]
-                severity_value = constants.RANKING_VALUES[severity]
-                try:
-                    sc = score['SEVERITY'][i] / severity_value
-                except ZeroDivisionError:
-                    sc = 0
-                totals[severity] = sc
-
-            machine_output['stats'].append({'filename': filer,
-                                            'score': self._sum_scores(score),
-                                            'issue totals': totals})
-
-        collector = self._get_issue_list()
-
-        if self.agg_type == 'vuln':
-            machine_output['results'] = sorted(collector,
-                                               key=itemgetter('error_type'))
-        else:
-            machine_output['results'] = sorted(collector,
-                                               key=itemgetter('filename'))
-
-        result = json.dumps(machine_output, sort_keys=True,
-                            indent=2, separators=(',', ': '))
-
-        if self.out_file:
-            with open(self.out_file, 'w') as fout:
-                fout.write(result)
-                # XXX: Should this be log output? (ukbelch)
-            print("JSON output written to file: %s" % self.out_file)
-        else:
-            print(result)
-
-    def _report_text(self, files_list, scores, excluded_files):
-        '''Prints the contents of the result store
-
-        :param files_list: Which files were inspected
-        :param scores: The scores awarded to each file in the scope
-        :param excluded_files: List of files excluded from the scope
-        :return: TXT string with appropriate TTY coloring for terminals
-        '''
-
-        tmpstr_list = []
-
-        # use a defaultdict to default to an empty string
-        color = defaultdict(str)
-
-        if self.format == 'txt':
-            # get text colors from settings for TTY output
-            get_setting = self.config.get_setting
-            color = {'HEADER': get_setting('color_HEADER'),
-                     'DEFAULT': get_setting('color_DEFAULT'),
-                     'LOW': get_setting('color_LOW'),
-                     'MEDIUM': get_setting('color_MEDIUM'),
-                     'HIGH': get_setting('color_HIGH')
-                     }
-
-        # print header
-        tmpstr_list.append("%sRun started:%s\n\t%s\n" % (
-            color['HEADER'],
-            color['DEFAULT'],
-            datetime.utcnow()
-        ))
-
-        if self.verbose:
-            # print which files were inspected
-            tmpstr_list.append("\n%sFiles in scope (%s):%s\n" % (
-                color['HEADER'], len(files_list),
-                color['DEFAULT']
-            ))
-
-            for item in zip(files_list, map(self._sum_scores, scores)):
-                tmpstr_list.append("\t%s (score: %i)\n" % item)
-
-            # print which files were excluded and why
-            tmpstr_list.append("\n%sFiles excluded (%s):%s\n" %
-                               (color['HEADER'], len(excluded_files),
-                                color['DEFAULT']))
-            for fname in excluded_files:
-                tmpstr_list.append("\t%s\n" % fname)
-
-        # print which files were skipped and why
-        tmpstr_list.append("\n%sFiles skipped (%s):%s\n" % (
-            color['HEADER'], len(self.skipped),
-            color['DEFAULT']
-        ))
-
-        for (fname, reason) in self.skipped:
-            tmpstr_list.append("\t%s (%s)\n" % (fname, reason))
-
-        # print the results
-        tmpstr_list.append("\n%sTest results:%s\n" % (
-            color['HEADER'], color['DEFAULT']
-        ))
-
-        if self.count == 0:
-            tmpstr_list.append("\tNo issues identified.\n")
-
-        for filename, issues in self.resstore.items():
-            for issue in issues:
-
-                # if the result isn't filtered out by severity
-                if self._check_severity(issue['issue_severity']):
-                    tmpstr_list.append("\n%s>> Issue: %s\n" % (
-                        color.get(issue['issue_severity'], color['DEFAULT']),
-                        issue['issue_text']
-                    ))
-                    tmpstr_list.append("   Severity: %s   Confidence: %s\n" % (
-                        issue['issue_severity'].capitalize(),
-                        issue['issue_confidence'].capitalize()
-                    ))
-                    tmpstr_list.append("   Location: %s:%s\n" % (
-                        issue['fname'],
-                        issue['lineno']
-                    ))
-                    tmpstr_list.append(color['DEFAULT'])
-
-                    tmpstr_list.append(
-                        self._get_code(issue, True))
-
-        result = ''.join(tmpstr_list)
-
-        if self.out_file:
-            with open(self.out_file, 'w') as fout:
-                fout.write(result)
-            self.logger.info("Text output written to file: %s", self.out_file)
-        else:
-            print(result)
-
     def _write_report(self, files_list, scores, excluded_files):
-        report_name = '_report_{}'.format(self.format)
-        report_func = getattr(self, report_name, self._report_text)
+        formatters_mgr = extension_loader.MANAGER.formatters_mgr
+        try:
+            formatter = formatters_mgr[self.format]
+        except KeyError:  # Unrecognized format, so use text instead
+            formatter = formatters_mgr['txt']
 
         if self.format == 'csv':
             self.max_lines = 1
-        elif report_func is self._report_text and self.out_file:
+        elif formatter.name == 'txt' and self.out_file:
             self.format = 'plain'
 
-        report_func(files_list, scores, excluded_files=excluded_files)
+        report_func = formatter.plugin
+        report_func(self, files_list, scores, excluded_files=excluded_files)
 
     def report(self, files_list, scores, excluded_files=None, lines=-1,
                level=1, output_filename=None, output_format=None):
