d82506ea4380a9a977ca83a9524ce0f08dcf3c13
Travis McPeak <travis.mcpeak@hp.com>
2015-07-14 08:34:41 -0600

    Modifying Paramiko Injection plugin
    The way the paramiko check was previously working didn't catch
    many uses because the qualified name is difficult to predict for
    Paramiko calls.  This commit changes it so that paramiko detection
    is accomplished by the combination of importing paramiko and using
    one of the paramiko functions that executes with a shell.
    
    Change-Id: Ib6e8f596a8f05475213be0ac8cf27e4e308b9617

---
+++ bandit/plugins/injection_paramiko.py
@@ -0,0 +1,29 @@
+# -*- coding:utf-8 -*-
+#
+# Copyright 2014 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import bandit
+from bandit.core.test_properties import *
+
+
+@checks('Call')
+def paramiko_calls(context):
+    issue_text = 'Probable Paramiko shell execution, check inputs'
+    for module in ['paramiko']:
+        if context.is_module_imported_like(module):
+            if context.call_function_name in ['exec_command', 'invoke_shell']:
+                return bandit.Issue(severity=bandit.MEDIUM,
+                                    confidence=bandit.MEDIUM,
+                                    text=issue_text)
