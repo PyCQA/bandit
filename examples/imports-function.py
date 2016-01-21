5180c65d26a0493cf7d49047fe7bd2b843e02653
Timothy Kelsey <tim.kelsey@hpe.com>
2016-01-21 14:48:06 +0000

    Fixing a bug and cleaning up in blacklisting code
    Closes-bug: 1536414
    Change-Id: I574e8c673c7a7dd197599286ec98f106a6d94fb5

--- examples/imports-function.py
+++ examples/imports-function.py
@@ -6,3 +6,7 @@ subprocess = __import__("subprocess")
 # this has been reported in the wild, though it's invalid python
 # see bug https://bugs.launchpad.net/bandit/+bug/1396333
 __import__()
+
+# TODO(??): bandit can not find this one unfortunatly (no symbol tab)
+a = 'subprocess'
+__import__(a)
