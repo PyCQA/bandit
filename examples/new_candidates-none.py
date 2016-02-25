421e032bc8aefa5cddbcdcd61cc9cb4bdbfc0f3f
Christopher J Schaefer <cjschaef@us.ibm.com>
2016-02-25 10:40:43 -0600

    Additional baseline candidate test coverage
    Additional test cases to improve functional test coverage for baseline
    candidate tests. Also includes example files for more complex candidate
    comparisons.
    
    Change-Id: I7d05c0f1e3f0d9e0a36c825654d28c19ed4c0bbd

---
+++ examples/new_candidates-none.py
@@ -0,0 +1,8 @@
+def subprocess_shell_cmd():
+    # sample function with known subprocess shell cmd candidates
+
+def yaml_load():
+    # sample function with known yaml.load candidates
+
+def xml_sax_make_parser():
+    # sample function with known xml.sax.make_parser candidates
