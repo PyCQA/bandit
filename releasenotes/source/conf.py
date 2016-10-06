006d873494957b040a513c731d896e4561966aef
Andreas Jaeger <aj@suse.com>
2016-10-06 20:24:26 +0200

    Enable release notes translation
    Releasenote translation publishing is being prepared. 'locale_dirs'
    needs to be defined in conf.py to generate translated version of the
    release notes.
    
    Note that this repository might not get translated release notes - or
    no translations at all - but we add the entry here nevertheless to
    prepare for it.
    
    Change-Id: I7bb3b22bd8801eb26651b58e434cd6c756c55c80

--- releasenotes/source/conf.py
+++ releasenotes/source/conf.py
@@ -272,3 +272,6 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 # texinfo_no_detailmenu = False
+
+# -- Options for Internationalization output ------------------------------
+locale_dirs = ['locale/']
