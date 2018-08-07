a9fe5e564d713099001ea11af04571d30a435f10
Tin Lam <stannum-l@users.noreply.github.com>
2018-08-07 18:38:06 -0500

    Add more_info URL to text output (#359)
    * Add more_info URL to text output
    
    Outputting bandit report as text does not put `more_info` URL while it
    would if the output format is YAML or JSON. This patch set adds the
    `more_info` URL to the text display.
    
    Signed-off-by: Tin Lam <tin@irrational.io>

--- tests/unit/formatters/test_text.py
+++ tests/unit/formatters/test_text.py
@@ -21,6 +21,7 @@ import testtools
 
 import bandit
 from bandit.core import config
+from bandit.core import docs_utils
 from bandit.core import issue
 from bandit.core import manager
 from bandit.formatters import text as b_text
@@ -45,7 +46,9 @@ class TextFormatterTests(testtools.TestCase):
                           format(_indent_val, _issue.severity.capitalize(),
                                  _issue.confidence.capitalize()),
                           "{}   Location: {}:{}".
-                          format(_indent_val, _issue.fname, _issue.lineno)]
+                          format(_indent_val, _issue.fname, _issue.lineno),
+                          "{}   More Info: {}".format(
+                              _indent_val, docs_utils.get_url(_issue.test_id))]
             if _code:
                 return_val.append("{}{}".format(_indent_val, _code))
             return '\n'.join(return_val)
