c185f38f6d79bbfeccc5afdd59cdfde6139944a6
Brian Williams <brcrwilliams@gmail.com>
2018-08-10 21:08:20 -0500

    Specify output_file encoding as utf-8

--- bandit/cli/main.py
+++ bandit/cli/main.py
@@ -215,7 +215,7 @@ def main():
     )
     parser.add_argument(
         '-o', '--output', dest='output_file', action='store', nargs='?',
-        type=argparse.FileType('w'), default=sys.stdout,
+        type=argparse.FileType('w', encoding='utf-8'), default=sys.stdout,
         help='write report to filename'
     )
     parser.add_argument(
