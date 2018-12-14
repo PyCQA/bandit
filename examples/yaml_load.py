ae32fc9b243b9fc39c962f92cef5b65926cb3ec4
Aaron Loo <aaronloo@yelp.com>
2018-12-14 08:34:30 -0800

    adding tests

--- examples/yaml_load.py
+++ examples/yaml_load.py
@@ -6,7 +6,11 @@ def test_yaml_load():
     ystr = yaml.dump({'a': 1, 'b': 2, 'c': 3})
     y = yaml.load(ystr)
     yaml.dump(y)
-    y = yaml.load(ystr, Loader=yaml.SafeLoader)
+    try:
+        y = yaml.load(ystr, Loader=yaml.CSafeLoader)
+    except AttributeError:
+        # CSafeLoader only exists if you build yaml with LibYAML
+        y = yaml.load(ystr, Loader=yaml.SafeLoader)
 
 
 def test_json_load():
