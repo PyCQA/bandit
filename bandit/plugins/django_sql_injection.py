a9aa26e66f700986b3f142035b929cec4635bbd6
Andrew MacFie <amacfie@sent.com>
2020-01-01 11:27:05 -0500

    Fix long lines

--- bandit/plugins/django_sql_injection.py
+++ bandit/plugins/django_sql_injection.py
@@ -36,7 +36,8 @@ def django_extra_used(context):
 
     .. seealso::
 
-     - https://docs.djangoproject.com/en/dev/topics/security/#sql-injection-protection
+     - https://docs.djangoproject.com/en/dev/topics/security/\
+#sql-injection-protection
 
     .. versionadded:: 1.5.0
 
@@ -98,7 +99,8 @@ def django_rawsql_used(context):
 
     .. seealso::
 
-     - https://docs.djangoproject.com/en/dev/topics/security/#sql-injection-protection
+     - https://docs.djangoproject.com/en/dev/topics/security/\
+#sql-injection-protection
 
     .. versionadded:: 1.5.0
 
