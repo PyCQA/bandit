126f9f5d8fa8cebe1e36dc0425f21e728292c2f1
Ved Sumedha Prabhu <prabhuved@gmail.com>
2020-09-04 01:18:29 +0530

    Update calls.py with additional rulesets
    Common vulnerabilities that I have spotted over the years due to the presence of vulnerable python libraries.

--- bandit/blacklists/calls.py
+++ bandit/blacklists/calls.py
@@ -568,5 +568,51 @@ def gen_blacklist():
         'Use of os.tempnam() and os.tmpnam() is vulnerable to symlink '
         'attacks. Consider using tmpfile() instead.'
         ))
+    
+    # updated rulesets starts here
+    
+    sets.append(utils.build_conf_dict(
+        'yaml_load', 'B326',
+        ['yaml.load'],
+        'Use of this deprecated deserialize method leads to remote code execution. See CVE-2017-18342.'
+        'Use yaml.safe_load instead.'
+        ))
 
+    sets.append(utils.build_conf_dict(
+        'response_splitting', 'B327',
+        ['urllib2.Request.add_header',
+        'urllib.URLopener.addheader',
+        'urllib.URLopener.addheaders'], 
+        'Python Urllib and Urllib2 are vulnerable to CRLF injection.'
+        'See https://bugs.python.org/issue36276'
+        ))
+    
+    sets.append(utils.build_conf_dict(
+        'Base64_encoding_used', 'B009',
+        ['base64.b64encode',
+        'base64.standard_b64encode',
+        'base64.urlsafe_b64encode',
+        'base64.b16encode',
+        'base64.b32encode',
+        'base64.encodestring',
+        'flask_scrypt.enbase64',
+        'base64.b64decode'],
+        'Weak encoding alogirthm. Encoding and storing sensitive information using this algorithm gives a false sense of security.',
+        'LOW'
+        ))
+    
+    sets.append(utils.build_conf_dict(
+        'Server_Side_Template_Injection', 'B017',
+        ['render_template_string', 'render_template'],
+        'User controlled Values injected into the template can lead to arbitrary code executions in the server. Check if input values are properly sanitized. See https://blog.nvisium.com/p263',
+        'MEDIUM'
+        ))
+    
+    sets.append(utils.build_conf_dict(
+        'file_upload', 'B024',
+        ['Flask.send_file'],
+        'Presence of a file upload funtionality. Ensure that the application whitelists the allowed file extensions and validated the MIME type.',
+        'MEDIUM'
+        ))
+    
     return {'Call': sets}
