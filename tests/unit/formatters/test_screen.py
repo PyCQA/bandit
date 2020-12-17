abf336648a2555bee51563e4faff7d5f8748d12d
Eric Brown <browne@vmware.com>
2020-12-16 23:36:51 -0800

    Show column offset on all formatters
    Recently, #618 introduced column offsets to the custom formatter.
    But other formatters should also show the column offset.
    
    Signed-off-by: Eric Brown <browne@vmware.com>

--- tests/unit/formatters/test_screen.py
+++ tests/unit/formatters/test_screen.py
@@ -35,8 +35,9 @@ class ScreenFormatterTests(testtools.TestCase):
                           "{}   Severity: {}   Confidence: {}".
                           format(_indent_val, _issue.severity.capitalize(),
                                  _issue.confidence.capitalize()),
-                          "{}   Location: {}:{}".
-                          format(_indent_val, _issue.fname, _issue.lineno),
+                          "{}   Location: {}:{}:{}".
+                          format(_indent_val, _issue.fname, _issue.lineno,
+                                 _issue.col_offset),
                           "{}   More Info: {}{}".format(
                               _indent_val, docs_utils.get_url(_issue.test_id),
                               screen.COLOR['DEFAULT'])]
@@ -56,6 +57,7 @@ class ScreenFormatterTests(testtools.TestCase):
         self.assertEqual(expected_return, issue_text)
 
         issue.lineno = ''
+        issue.col_offset = ''
         issue_text = screen._output_issue_str(issue, indent_val,
                                               show_lineno=False)
         expected_return = _template(issue, indent_val, 'DDDDDDD',
