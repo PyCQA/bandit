b72faa39e7e1c2d0af395598aac515bbbc3c43c5
asears <asears@users.noreply.github.com>
2020-12-20 16:18:50 -0500

    shorten description to address 80 character limit
    Signed-off-by: asears <asears@users.noreply.github.com>

--- bandit/__main__.py
+++ bandit/__main__.py
@@ -3,9 +3,9 @@
 """Bandit is a tool designed to find common security issues in Python code.
 
 Bandit is a tool designed to find common security issues in Python code.
-To do this Bandit processes each file, builds an AST from it, and runs appropriate
-plugins against the AST nodes. Once Bandit has finished scanning all the files
-it generates a report.
+To do this Bandit processes each file, builds an AST from it, and runs
+appropriate plugins against the AST nodes. Once Bandit has finished
+scanning all the files it generates a report.
 
 Bandit was originally developed within the OpenStack Security Project and
 later rehomed to PyCQA.
