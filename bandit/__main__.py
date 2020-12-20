6a281ded132d8135e93a747145eb02e069c8505a
Andrew Sears <asears@users.noreply.github.com>
2020-12-20 16:32:08 -0500

    Updates to address docstring code scan issues, add flake8 configuration (#671)
    * Updates to address docstring code scan issues, add flake8 configuration
    
    Signed-off-by: asears <asears@users.noreply.github.com>
    
    * Update .flake8
    
    Co-authored-by: Eric Brown <ericwb@users.noreply.github.com>
    
    * Shorthand SPDX license one line header
    
    Signed-off-by: asears <asears@users.noreply.github.com>
    
    * update main for single-line SPDX and remove additional comment
    
    Signed-off-by: asears <asears@users.noreply.github.com>
    
    * revert init py docstrings.
    
    Signed-off-by: asears <asears@users.noreply.github.com>
    
    * Revert headings after PR review
    
    Signed-off-by: asears <asears@users.noreply.github.com>
    
    * remove 120 character limit setting
    
    Signed-off-by: asears <asears@users.noreply.github.com>
    
    * shorten description to address 80 character limit
    
    Signed-off-by: asears <asears@users.noreply.github.com>
    
    Co-authored-by: Eric Brown <ericwb@users.noreply.github.com>

--- bandit/__main__.py
+++ bandit/__main__.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python
+# SPDX-License-Identifier: Apache-2.0
+"""Bandit is a tool designed to find common security issues in Python code.
+
+Bandit is a tool designed to find common security issues in Python code.
+To do this Bandit processes each file, builds an AST from it, and runs
+appropriate plugins against the AST nodes. Once Bandit has finished
+scanning all the files it generates a report.
+
+Bandit was originally developed within the OpenStack Security Project and
+later rehomed to PyCQA.
+
+https://bandit.readthedocs.io/
+"""
 from bandit.cli import main
 main.main()
