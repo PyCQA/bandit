826750b4963870b03dd09f6445594f2f2e8e95d7
Maciej Strömich <maciej.stromich@gmail.com>
2021-03-31 16:20:31 +0200

    improvement in the tests

--- examples/imports-with-importlib.py
+++ examples/imports-with-importlib.py
@@ -9,5 +9,7 @@ e = importlib.import_module(MODULE_MAP[key])
 f = importlib.__import__(MODULE_MAP[key])
 
 # Do not crash when target is a named argument
-g = importlib.import_module(name='foo', package='bar.baz')
-h = importlib.__import__(name='foo', package='bar.baz')
+g = importlib.import_module(name='sys')
+h = importlib.__import__(name='subprocess')
+i = importlib.import_module(name='subprocess', package='bar.baz')
+j = importlib.__import__(name='sys', package='bar.baz')
