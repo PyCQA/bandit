9589ca76731b43ad0e4c8efad5df4d22951b5764
David Melamed <dvdmelamed@gmail.com>
2021-09-23 14:08:06 +0300

    Update file

--- examples/eval.py
+++ examples/eval.py
@@ -4,6 +4,7 @@ print(eval("1+1"))
 print(eval("os.getcwd()"))
 print(eval("os.chmod('%s', 0777)" % 'test.txt'))
 
+exec("do evil")
 
 # A user-defined method named "eval" should not get flagged.
 class Test(object):
