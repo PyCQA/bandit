130a467434ff6b8f9bbd83100e409bc1fcb6f407
Eric Brown <ericwb@users.noreply.github.com>
2022-03-18 17:49:12 -0700

    Removal of blacklist call B309 httpsconnection (#858)
    This check existed because of insufficient checking of certificates
    when using httpsconnection. Since 3.4.3, this has been fixed. And
    since Bandit supports 3.7+, there is no longer a need to scan for
    this.
    
    Closes #857
    
    Signed-off-by: Eric Brown <browne@vmware.com>

--- examples/httplib_https.py
+++
@@ -1,8 +0,0 @@
-import httplib
-c = httplib.HTTPSConnection("example.com")
-
-import http.client
-c = http.client.HTTPSConnection("example.com")
-
-import six
-six.moves.http_client.HTTPSConnection("example.com")
