5ff73ff8ff956df7d63fde49c3bd671db8e821eb
Moritz <37435502+mschfh@users.noreply.github.com>
2022-03-28 06:29:29 +0200

    add check for "requests" calls without timeout (#743)
    * add check for "requests" calls without timeout
    
    * change request_without_timeout confidence to low
    
    * Update bandit/plugins/request_without_timeout.py
    
    Co-authored-by: Eric Brown <ericwb@users.noreply.github.com>
    
    * Update bandit/plugins/request_without_timeout.py
    
    Co-authored-by: Eric Brown <ericwb@users.noreply.github.com>
    
    * Update doc/source/plugins/b113_request_without_timeout.rst
    
    Co-authored-by: Eric Brown <ericwb@users.noreply.github.com>
    
    * Update doc/source/plugins/b113_request_without_timeout.rst
    
    Co-authored-by: Eric Brown <ericwb@users.noreply.github.com>
    
    * Update bandit/plugins/request_without_timeout.py
    
    Co-authored-by: Eric Brown <ericwb@users.noreply.github.com>
    
    * remove utf-8
    
    * fix confidence in comment
    
    * Apply suggestions from code review
    
    * Update issue.py
    
    * Apply suggestions from code review
    
    * Apply suggestions from code review
    
    * Apply suggestions from code review
    
    * Apply suggestions from code review
    
    Co-authored-by: Eric Brown <ericwb@users.noreply.github.com>

--- tests/functional/test_functional.py
+++ tests/functional/test_functional.py
@@ -393,6 +393,14 @@ class FunctionalTests(testtools.TestCase):
         }
         self.check_example("requests-ssl-verify-disabled.py", expect)
 
+    def test_requests_without_timeout(self):
+        """Test for the `requests` library missing timeouts."""
+        expect = {
+            "SEVERITY": {"UNDEFINED": 0, "LOW": 0, "MEDIUM": 14, "HIGH": 0},
+            "CONFIDENCE": {"UNDEFINED": 0, "LOW": 14, "MEDIUM": 0, "HIGH": 0},
+        }
+        self.check_example("requests-missing-timeout.py", expect)
+
     def test_skip(self):
         """Test `#nosec` and `#noqa` comments."""
         expect = {
