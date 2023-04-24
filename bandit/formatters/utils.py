f6472654e3b2fce257434c30e3803688300e4878
Michal Porteš <michalportes1@gmail.com>
2023-04-24 14:10:37 +0200

    Simplify `wrap_file_object`
    Remove code that is specific to Python 2.

--- bandit/formatters/utils.py
+++ bandit/formatters/utils.py
@@ -6,18 +6,9 @@ import io
 
 
 def wrap_file_object(fileobj):
-    """Handle differences in Python 2 and 3 around writing bytes."""
-    # If it's not an instance of IOBase, we're probably using Python 2 and
-    # that is less finnicky about writing text versus bytes to a file.
-    if not isinstance(fileobj, io.IOBase):
-        return fileobj
-
-    # At this point we're using Python 3 and that will mangle text written to
-    # a file written in bytes mode. So, let's check if the file can handle
-    # text as opposed to bytes.
+    """If the fileobj passed in cannot handle text, use TextIOWrapper
+    to handle the conversion.
+    """
     if isinstance(fileobj, io.TextIOBase):
         return fileobj
-
-    # Finally, we've determined that the fileobj passed in cannot handle text,
-    # so we use TextIOWrapper to handle the conversion for us.
     return io.TextIOWrapper(fileobj)
