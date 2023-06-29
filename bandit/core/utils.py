db6d677dab5a374e1d7b0768897e5f35d372d2d9
Eric Brown <ericwb@users.noreply.github.com>
2023-06-29 10:01:26 -0700

    Remove support for Python 3.7 due to end-of-life (#1034)
    Python 3.7 was end-of-life as of 6-27-2023. As such, there is no
    need for Bandit to continue to support it.
    
    Fixes #992
    
    Signed-off-by: Eric Brown <eric_wade_brown@yahoo.com>

--- bandit/core/utils.py
+++ bandit/core/utils.py
@@ -219,7 +219,7 @@ def calc_linerange(node):
 
 def linerange(node):
     """Get line number range from a node."""
-    if sys.version_info >= (3, 8) and hasattr(node, "lineno"):
+    if hasattr(node, "lineno"):
         return list(range(node.lineno, node.end_lineno + 1))
     else:
         if hasattr(node, "_bandit_linerange_stripped"):
