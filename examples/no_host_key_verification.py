f8161ebf402c6fc0af00bd55d77e6725ee1fd90d
Mathieu Kniewallner <mathieu.kniewallner@gmail.com>
2023-09-25 20:27:53 +0200

    fix(plugins/B507): also detect class instances
    `paramiko` supports passing both a class and a class instance for the
    policy in `set_missing_host_key_policy`
    (https://github.com/paramiko/paramiko/blob/8e389c77660c5cdae3069b478665427d23012853/paramiko/client.py#L171-L191).
    This updates B507 to account for both styles.

--- examples/no_host_key_verification.py
+++ examples/no_host_key_verification.py
@@ -3,3 +3,5 @@ from paramiko import client
 ssh_client = client.SSHClient()
 ssh_client.set_missing_host_key_policy(client.AutoAddPolicy)
 ssh_client.set_missing_host_key_policy(client.WarningPolicy)
+ssh_client.set_missing_host_key_policy(client.AutoAddPolicy())
+ssh_client.set_missing_host_key_policy(client.WarningPolicy())
