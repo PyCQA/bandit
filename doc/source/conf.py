7cd9e7beac97acd0ed1fdcd00f2978ca1ba4f21f
Eric Brown <ericwb@users.noreply.github.com>
2023-12-08 09:56:59 -0800

    Update conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -2,7 +2,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../../bandit"))
+sys.path.insert(0, os.path.abspath("../.."))
 # -- General configuration ----------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
