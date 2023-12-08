8ef9b92649f7b5a00012bf669e9e86dee578173e
Eric Brown <ericwb@users.noreply.github.com>
2023-12-08 12:32:42 -0800

    Update __init__.py

--- bandit/__init__.py
+++ bandit/__init__.py
@@ -1,1 +1,22 @@
+#
+# Copyright 2014 Hewlett-Packard Development Company, L.P.
+#
+# SPDX-License-Identifier: Apache-2.0
+try:
+    from importlib import metadata
+except ImportError:
+    import importlib_metadata as metadata
 
+from bandit.core import config  # noqa
+from bandit.core import context  # noqa
+from bandit.core import manager  # noqa
+from bandit.core import meta_ast  # noqa
+from bandit.core import node_visitor  # noqa
+from bandit.core import test_set  # noqa
+from bandit.core import tester  # noqa
+from bandit.core import utils  # noqa
+from bandit.core.constants import *  # noqa
+from bandit.core.issue import *  # noqa
+from bandit.core.test_properties import *  # noqa
+
+__version__ = metadata.version("bandit")
