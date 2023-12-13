5ec806de1901fe99083ddeab4b117c2ccf84c2e9
Eric Brown <ericwb@users.noreply.github.com>
2023-12-12 19:49:47 -0800

    Handle variant in how policy is passed in paramiko (#1078)
    Paramiko permits various ways of importing the missing host key
    policy. It allows paramiko.client.AutoAddPolicy or paramiko.AutoAddPolicy.
    The later isn't being handled in Bandit.
    
    This change adds news tests and modifies the plugin to inspect the
    AST to determine whether the argument is an Attribute, Name, or
    Call.
    
    Fixes #1077
    
    Signed-off-by: Eric Brown <eric_wade_brown@yahoo.com>

--- examples/no_host_key_verification.py
+++ examples/no_host_key_verification.py
@@ -1,7 +1,14 @@
 from paramiko import client
+from paramiko import AutoAddPolicy
+from paramiko import WarningPolicy
 
 ssh_client = client.SSHClient()
 ssh_client.set_missing_host_key_policy(client.AutoAddPolicy)
 ssh_client.set_missing_host_key_policy(client.WarningPolicy)
 ssh_client.set_missing_host_key_policy(client.AutoAddPolicy())
 ssh_client.set_missing_host_key_policy(client.WarningPolicy())
+
+ssh_client.set_missing_host_key_policy(AutoAddPolicy)
+ssh_client.set_missing_host_key_policy(WarningPolicy)
+ssh_client.set_missing_host_key_policy(AutoAddPolicy())
+ssh_client.set_missing_host_key_policy(WarningPolicy())
