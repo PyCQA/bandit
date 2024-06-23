a339b1593dbab632b560518777c261c45616151c
Eric Brown <eric_wade_brown@yahoo.com>
2024-06-22 21:08:23 -0700

    Add test for usage of FTP_TLS
    This change adds an FTP_TLS call to the examples. A high severity
    error is no longer reported as a result of the fix in PR #1148
    that explicitly now matches blacklist call qualified names rather
    than using a file glob.
    
    However, you will notice that there is one more high severity
    issue reported in the tests as a result of the import of
    ftplib.FTP_TLS because the blacklist import is only checking for
    "ftplib".
    
    Fixes: #148
    
    Signed-off-by: Eric Brown <eric_wade_brown@yahoo.com>

--- examples/ftplib.py
+++ examples/ftplib.py
@@ -1,9 +1,24 @@
 from ftplib import FTP
+from ftplib import FTP_TLS
 
+
+# bad
 ftp = FTP('ftp.debian.org')
 ftp.login()
 
 ftp.cwd('debian')
 ftp.retrlines('LIST')
+
+ftp.quit()
+
+# okay
+ftp = ftplib.FTP_TLS(
+    "ftp.us.debian.org",
+    context=ssl.create_default_context(),
+)
+ftp.login()
+
+ftp.cwd("debian")
+ftp.retrlines("LIST")
 
 ftp.quit()
