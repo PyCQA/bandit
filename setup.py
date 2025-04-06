2093d021fe1227cae200c8845e81600737d49c06
Eric Brown <eric_wade_brown@yahoo.com>
2025-04-06 16:33:47 -0700

    Ensure the man page is built
    Ever since Sphinx switched to sphinx-build command to build
    docs, the man page was no longer being built. This is because
    there is now a separate subcommand "sphinx-build -b man" to
    generate the man page file.
    
    This change adds the separate command and ensures our publish
    workflows generate the file when releasing a new bandit version.
    
    This also adds the man page as part of the data files so it
    gets included with the wheel and is installed to share/man/man1.
    
    Signed-off-by: Eric Brown <eric_wade_brown@yahoo.com>

--- setup.py
+++ setup.py
@@ -1,8 +1,20 @@
 # Copyright (c) 2013 Hewlett-Packard Development Company, L.P.
 #
 # SPDX-License-Identifier: Apache-2.0
+import os
+
 import setuptools
 
+
+data_files = []
+man_path = "doc/build/man/bandit.1"
+if os.path.isfile(man_path):
+    data_files.append(("share/man/man1", [man_path]))
+
+
 setuptools.setup(
-    python_requires=">=3.9", setup_requires=["pbr>=2.0.0"], pbr=True
+    python_requires=">=3.9",
+    setup_requires=["pbr>=2.0.0"],
+    pbr=True,
+    data_files=data_files,
 )
