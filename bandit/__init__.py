cb346e259d2f892c33c6386862cd4e31f18782c7
Daniel Mohr <daniel.mohr@dlr.de>
2025-06-30 16:06:57 +0200

    typo

--- bandit/__init__.py
+++ bandit/__init__.py
@@ -18,7 +18,6 @@ from bandit.core.test_properties import *  # noqa
 
 __author__ = metadata.metadata("bandit")["Author"]
 __version__ = metadata.version("bandit")
-# __version__ = metadata.version(__package__)
 # running bandit inside pre-commit we do not get a version here, workaround:
 if __version__ == "0.0.0":
     __version__ = "latest"
